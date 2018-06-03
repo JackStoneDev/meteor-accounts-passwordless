d023304f31bfd507b855089727014416777642cb
Jack Stone <jack@jackstonedev.com>
2018-06-03 17:36:12 -0400

    Remove demo.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jackstone:accounts-passwordless',
-  version: '0.2.6',
+  version: '0.2.7',
   summary: 'Token-based one-time password (OTPW) authentication (nopassword, passwordless)',
   git: 'https://github.com/JackStoneDev/meteor-accounts-passwordless',
   documentation: 'README.md'
